48b81b063efdb9dd7f8d3586a167105885c0c193
Meraj Syed <merajhus@opentext.com>
2023-12-26 10:53:13 +0530

    created a simple react app

---
+++ scientist-gallery/src/gallery.js
@@ -0,0 +1,13 @@
+import Profile from './profile'
+ 
+ 
+   export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists gallery</h1>
+        <Profile />
+        <Profile />
+        <Profile />
+      </section>
+    );
+  }
---
+++ scientist-gallery/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import Gallery  from './gallery';
+
+  
+
+  
+  
+  const container = document.getElementById("root");
+  const root = ReactDOM.createRoot(container);
+  root.render(<Gallery />);
+  
+  // ========================================
+  
+  //const root = ReactDOM.createRoot(document.getElementById("root"));
+  //root.render(<Game />);
+  
