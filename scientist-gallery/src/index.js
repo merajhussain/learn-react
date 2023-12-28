c351b53c1b26b342a5ec4faaa7c485f034552751
Meraj Hussain Syed <merajhussain1@gmail.com>
2023-12-28 15:18:16 +0530

    use of state and hook

--- scientist-gallery/src/index.js
+++ scientist-gallery/src/index.js
@@ -1,18 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
-import Gallery  from './gallery';
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import { useState } from "react";
+import { useEffect } from "react";
 
-  
 
-  
-  
-  const container = document.getElementById("root");
-  const root = ReactDOM.createRoot(container);
-  root.render(<Gallery />);
-  
-  // ========================================
-  
-  //const root = ReactDOM.createRoot(document.getElementById("root"));
-  //root.render(<Game />);
-  
+function PizzaApp()
+{
+   return (
+     <div> 
+        <Header/>
+        <Menu/>
+        <Footer/>
+     </div>
+   );
+}
+
+function Header()
+{
+return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu()
+{
+   return (
+    <div>
+        <ul>
+            <li>Pizza1</li>
+            <li>Pizza2</li>
+            <li>Pizza3</li>
+        </ul>
+    </div>
+   );
+}
+
+function Footer()
+{
+    const [currTIme, setCurrTime] = useState(new Date().toLocaleTimeString());
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        setTimeout(() => {
+            setCurrTime((currTIme) => new Date().toLocaleTimeString());
+        }, 1000);
+      });
+    
+return <footer>{currTIme}. We're currenly open</footer>
+}
+
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<PizzaApp/>);
